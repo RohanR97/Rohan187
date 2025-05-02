17e67f363774312e30862909b1aab4f99aab9afc
rohan <rohan@DESKTOP-10OLR58>
2025-05-02 16:17:06 +0530

    Made changes.

--- GitProject2/src/com/app/Data.java
+++ GitProject2/src/com/app/Data.java
@@ -2,6 +2,12 @@ package com.app;
 
 public class Data {
 	
+	
+	public void m1()
+	{
+		System.out.println("Hello.......");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("This a git program.");
 
