0feac1662a766568a51c83f3a3716132b5a59dce
RohanR97 <rohanrangari123@gmail.com>
2025-05-02 16:12:13 +0530

    Update Data.java

--- GitProject2/src/com/app/Data.java
+++ GitProject2/src/com/app/Data.java
@@ -1,7 +1,12 @@
 package com.app;
 
 public class Data {
-	
+
+
+	public void m2()
+	{
+		System.out.println("Hello Again");
+	}
 	public static void main(String[] args) {
 		System.out.println("This a git program.");
 
